07c09cc31186788554bae2379ce61bc98302d3c7
Greqory <gregor.thume@gmail.com>
2024-04-21 20:20:04 +0200

    adding tmux plugins

--- config/home/tmux.nix
+++ config/home/tmux.nix
@@ -8,7 +8,34 @@
     plugins = with pkgs;
       [
         tmuxPlugins.better-mouse-mode
+
+        # must be before continuum edits right status bar
+        { 
+          plugin = tmuxPlugins.catppuccin;
+          extraConfig = '' 
+          set -g @catppuccin_flavour 'frappe'
+          set -g @catppuccin_window_tabs_enabled on
+          set -g @catppuccin_date_time "%H:%M"
+          '';
+        }
+        {
+          plugin = tmuxPlugins.resurrect;
+          extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+          '';
+        }
+        {
+          plugin = tmuxPlugins.continuum;
+          extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-boot 'on'
+          set -g @continuum-save-interval '10'
+          '';
+        }        
       ];
+
     extraConfig = ''
     '';
   };
