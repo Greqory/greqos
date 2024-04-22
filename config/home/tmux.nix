9a8a95d9a6809b8759d074f65809380adfdb00fa
Greqory <gregor.thume@gmail.com>
2024-04-22 11:04:25 +0200

    adding tmux plugins

--- config/home/tmux.nix
+++ config/home/tmux.nix
@@ -31,7 +31,7 @@
           extraConfig = ''
           set -g @continuum-restore 'on'
           set -g @continuum-boot 'on'
-          set -g @continuum-save-interval '10'
+          set -g @continuum-save-interval '2'
           '';
         }        
       ];
