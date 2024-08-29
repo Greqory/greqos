1698a482d288116479454ed12c7f2f6a22ff3607
Greqory <gregor.thume@gmail.com>
2024-08-29 11:31:10 +0200

    adding pip

--- config/system/packages.nix
+++ config/system/packages.nix
@@ -24,7 +24,7 @@
     #linuxKernel.packages.linux_xanmod_latest.vmware
     rustc rustscan rustcat gcc elf2uf2-rs glibc glib
     dbus c-periphery python3Full gob2 python311Packages.pygobject3 gobject-introspection python311Packages.wheel
-    jetbrains.pycharm-community dia
+    jetbrains.pycharm-community dia pip
   ];
 
   programs = {
