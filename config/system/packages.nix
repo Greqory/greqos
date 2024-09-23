639f41ec07ee689b364b8b8562e4b339a8b6ef2d
Greqory <gregor.thume@gmail.com>
2024-09-23 18:43:58 +0200

    adding glibc_multi

--- config/system/packages.nix
+++ config/system/packages.nix
@@ -22,7 +22,7 @@
     #vmware-workstation
     #linuxKernel.packages.linux_xanmod_latest.phc-intel
     #linuxKernel.packages.linux_xanmod_latest.vmware
-    rustc rustscan rustcat gcc elf2uf2-rs glibc glib glibc
+    rustc rustscan rustcat gcc elf2uf2-rs glibc glib glibc_multi
     dbus c-periphery python3Full gob2 python311Packages.pygobject3 gobject-introspection python311Packages.wheel
     jetbrains.pycharm-community dia pipx
   ];
