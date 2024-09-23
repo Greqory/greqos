ff5c7b27391b52000028fa077637efaed1d6ab8c
Greqory <gregor.thume@gmail.com>
2024-09-23 19:26:58 +0200

    adding more stuff

--- config/system/packages.nix
+++ config/system/packages.nix
@@ -22,7 +22,8 @@
     #vmware-workstation
     #linuxKernel.packages.linux_xanmod_latest.phc-intel
     #linuxKernel.packages.linux_xanmod_latest.vmware
-    rustc rustscan rustcat gcc elf2uf2-rs glibc glib glibc_multi gdb python312Packages.pyserial doxygen gccMultiStdenv
+    rustc rustscan rustcat gcc elf2uf2-rs glibc glib glibc_multi gdb python312Packages.pyserial doxygen gccMultiStdenv openocd
+    libstdcxx5 gcc_multi gcc-arm-embedded libgcc mkspiffs-presets.esp-idf esptool qemu
     dbus c-periphery python3Full gob2 python311Packages.pygobject3 gobject-introspection python311Packages.wheel
     jetbrains.pycharm-community dia pipx
   ];
