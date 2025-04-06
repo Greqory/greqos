459195a9a9cefb5977cb42360681e65d128f68ab
Greqory <gregor.thume@gmail.com>
2025-04-06 11:36:36 +0200

    removing catppuccin login

--- config/system/displaymanager.nix
+++ config/system/displaymanager.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, host, ... }:
+{ pkgs,  host, ... }:
 
 let inherit (import ../../hosts/${host}/options.nix) theKBDVariant
 theKBDLayout theSecondKBDLayout; in
@@ -12,25 +12,20 @@ theKBDLayout theSecondKBDLayout; in
   };
   services.libinput.enable = true;
 
-
   services.displayManager.sddm = {
       enable = true;
       autoNumlock = true;
       wayland.enable = true;
-      #theme = "tokyo-night-sddm";
-      theme = "sddm-catppuccin";
+      theme = "tokyo-night-sddm";
   };
-
 
   environment.systemPackages =
   let
-    sddm-catppuccin = pkgs.callPackage ../pkgs/sddm-catppuccin.nix {};
     sugar = pkgs.callPackage ../pkgs/sddm-sugar-dark.nix {};
     tokyo-night = pkgs.libsForQt5.callPackage ../pkgs/sddm-tokyo-night.nix {};
 in [ 
     sugar.sddm-sugar-dark # Name: sugar-dark
     tokyo-night # Name: tokyo-night-sddm
-    sddm-catppuccin
     pkgs.libsForQt5.qt5.qtgraphicaleffects
     pkgs.qt6.full
     pkgs.qt6.qtsvg
